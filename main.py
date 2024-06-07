ef2ae1ada70c3aab2e26b03cee2cb2e5c28c7c61
top34051 <top34051@gmail.com>
2024-06-07 01:58:41 -0700

    Update

--- main.py
+++ main.py
@@ -1,46 +1,87 @@
 import streamlit as st
-import pandas as pd
-import soundfile as sf
-import numpy as np
-import io
+import os
+import json
 
-st.set_page_config(layout="wide")
+st.set_page_config(page_title="ASR LibriStutter", layout="wide")
 
-# Function to load audio file
-def load_audio(file):
-    try:
-        data, samplerate = sf.read(file)
-        return data, samplerate
-    except Exception as e:
-        st.error(f"Error loading audio file: {e}")
-        return None, None
-
-# Demo data (you can replace these with your actual data)
-audio_file = '5678-43303-0036.flac'
-groundtruth_transcript = "I'm back all is well now listen can you hear yes the best has happened it is all over in the East felsenburgh has done it now listen I cannot come home tonight it will be announced in Paul's house in 2 hours from now"
-wav2vec_transcript = "I AM BACK ALL AS WELL NOW LISTEN I WELL NOW LISTEN WELL NOW LISTEN CAN YOU HEAR YES YES THE BEST HAS HAPPENED IT IS IIS ALL OVER IN THE EAST FELSENBURGH HAS DONE IT NOW LISTEN I CANNOT COME HOME TO NIGHT IT WILL WILL WILL BE ANNOUNCED IN PAUL'S HOUSE IN TWO HOURS FROM NOW"
-whisper_transcript = "I'm back all as well. Now listen. Well, now listen. Well, now listen. Can you hear? Yes, yes. The best has happened. It is I'm is all over in the east. Thelsonburg has done it. Now listen, I cannot come home tonight. It will will be announced in Paul's house in two hours from now."
-whispering_llama_transcript = "I'm back all as well. Now listen. Well, now listen. Well, now listen. Can you hear? Yes, yes. The best has happened. It is I'm is all over in the east. Thelsonburg has done it. Now listen, I cannot come home tonight. It will will be announced in Paul's house in two hours from now."
-ours_transcript = "I'm back all is well now listen can you hear yes the best has happened it is all over in the East felsenburgh has done it now listen I cannot come home tonight it will be announced in Paul's house in 2 hours from now"
-
-# Streamlit app
-st.title("Stuttered Speech Recognition Demo")
-
-# Audio Playback
-st.header("Audio Playback")
-audio_data, audio_samplerate = load_audio(audio_file)
-if audio_data is not None:
-    st.audio(audio_file, format='audio/wav')
-
-# Transcriptions
-st.header("Transcriptions")
-transcriptions = {
-    "Groundtruth": groundtruth_transcript,
-    "Wav2Vec": wav2vec_transcript,
-    "Whisper": whisper_transcript,
-    "Whispering Llama": whispering_llama_transcript,
-    "Ours": ours_transcript,
+files = os.listdir("asr_outputs")
+file_to_name = {
+    "whisper_tiny.json": "Whisper (Tiny)",
+    "whisper_tiny_gpt-4o_1refs.json": "Whisper (Tiny) &rarr; GPT-4o (1 Refs)",
+    "whisper_tiny_gpt-4o_5refs.json": "Whisper (Tiny) &rarr; GPT-4o (5 Refs)",
+    "whisper_tiny_gpt-4o_10refs.json": "Whisper (Tiny) &rarr; GPT-4o (10 Refs)",
+    "whisper_tiny_llama_2_1refs.json": "Whisper (Tiny) &rarr; Llama 2 (1 Refs)",
+    "whisper_tiny_llama_2_5refs.json": "Whisper (Tiny) &rarr; Llama 2 (5 Refs)",
+    "whisper_tiny_llama_2_10refs.json": "Whisper (Tiny) &rarr; Llama 2 (10 Refs)",
+    "whisper_small.json": "Whisper (Small)",
+    "whisper_small_gpt-4o_1refs.json": "Whisper (Small) &rarr; GPT-4o (1 Refs)",
+    "whisper_small_gpt-4o_5refs.json": "Whisper (Small) &rarr; GPT-4o (5 Refs)",
+    "whisper_small_gpt-4o_10refs.json": "Whisper (Small) &rarr; GPT-4o (10 Refs)",
+    "whisper_base.json": "Whisper (Base)",
+    "whisper_base_gpt-4o_1refs.json": "Whisper (Base) &rarr; GPT-4o (1 Refs)",
+    "whisper_base_gpt-4o_5refs.json": "Whisper (Base) &rarr; GPT-4o (5 Refs)",
+    "whisper_base_gpt-4o_10refs.json": "Whisper (Base) &rarr; GPT-4o (10 Refs)",
+    "whisper_medium.json": "Whisper (Medium)",
+    "whisper_medium_gpt-4o_1refs.json": "Whisper (Medium) &rarr; GPT-4o (1 Refs)",
+    "whisper_medium_gpt-4o_5refs.json": "Whisper (Medium) &rarr; GPT-4o (5 Refs)",
+    "whisper_medium_gpt-4o_10refs.json": "Whisper (Medium) &rarr; GPT-4o (10 Refs)",
+    "whisper_medium_llama_2_1refs.json": "Whisper (Medium) &rarr; Llama 2 (1 Refs)",
+    "whisper_medium_llama_2_5refs.json": "Whisper (Medium) &rarr; Llama 2 (5 Refs)",
+    "whisper_medium_llama_2_10refs.json": "Whisper (Medium) &rarr; Llama 2 (10 Refs)",
 }
 
-for name, transcript in transcriptions.items():
-    st.info(f"**{name}**\n\n{transcript}")
+st.write("**Automatic Speech Recognition (ASR) Outputs on LibriStutter**")
+
+displaying_files = st.sidebar.multiselect(
+    "Select files to display",
+    files,
+    default=[
+        "whisper_small.json",
+        "whisper_base.json",
+        "whisper_medium.json",
+        "whisper_medium_gpt-4o_1refs.json",
+        "whisper_medium_gpt-4o_5refs.json",
+        "whisper_medium_gpt-4o_10refs.json",
+        "whisper_medium_llama_2_1refs.json",
+        "whisper_medium_llama_2_5refs.json",
+        "whisper_medium_llama_2_10refs.json",
+    ],
+    format_func=lambda x: file_to_name[x],
+)
+
+sample_id = st.sidebar.selectbox("Select sample", range(10))
+
+data = {
+    file_to_name[file]: json.load(open(f"asr_outputs/{file}"))[sample_id]["transcript"] for file in displaying_files
+}
+
+items = list(data.items())
+
+ground_truth_value = json.load(open(f"asr_outputs/{displaying_files[0]}"))[sample_id]["ground_truth"]
+st.markdown(
+    f"""
+    <div style="border: 2px solid; border-radius: 6px; padding: 20px 20px 20px 20px; margin: 10px 0; background-color: #cce7ff;">
+        <h5 style="margin: 0;">Ground truth</h4>
+        <p style="margin: 0;">{ground_truth_value}</p>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
+
+num_cols = 3
+num_rows = (len(items) + num_cols - 1) // num_cols
+for row in range(num_rows):
+    cols = st.columns(num_cols)
+    for col in range(num_cols):
+        idx = row * num_cols + col
+        if idx < len(items):
+            key, value = items[idx]
+            cols[col].markdown(
+                f"""
+            <div style="border: 2px solid #ddd; border-radius: 6px; padding: 20px 20px 20px 20px; margin: 10px 0; width: 100%;">
+                <h5 style="margin: 0;">{key}</h4>
+                <p style="margin: 0;">{value}</p>
+            </div>
+            """,
+                unsafe_allow_html=True,
+            )
